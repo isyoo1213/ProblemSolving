f8f43a3a0516005f361a73e256d87039ae893e7f
isyoo1213 <moethns@gmail.com>
2023-08-10 21:10:44 +0900

    **private-BasicPS-MinMax-BRusingIf

---
+++ minMax/Ps1BasicBRIf.java
@@ -0,0 +1,46 @@
+package main.sourcecode.basicPS.minMax;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 문제
+ * N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+ *
+ * 입력
+ * 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+ *
+ * 출력
+ * 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+ *
+ * 예제 입력 1
+ * 5
+ * 20 10 35 30 7
+ * 예제 출력 1
+ * 7 35
+ */
+
+public class Ps1BasicBRIf {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int max = -1000001;
+        int min = 1000001;
+
+        while (st.hasMoreTokens()) {
+            int val = Integer.parseInt(st.nextToken());
+            if (val > max) {
+                max = val;
+            }
+            if (val < min) {
+                min = val;
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+}
