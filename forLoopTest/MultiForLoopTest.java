aac9cbc4aadd38914694a2204682ad7684047609
isyoo1213 <moethns@gmail.com>
2023-08-20 23:46:57 +0900

    *private-BasicPS-ForLoopTest-run2

--- forLoopTest/MultiForLoopTest.java
+++ forLoopTest/MultiForLoopTest.java
@@ -19,9 +19,27 @@ public class MultiForLoopTest {
         return resultTime;
     }
 
+    public static long run2() {
+        final long start = System.nanoTime();
+        long count = 0;
+        for(int i = 0; i<100; i++) {
+
+            for(int j = 0 ; j<1000000000 ; j++) {
+                count++;
+            }
+        }
+
+        final long end = System.nanoTime();
+        final long resultTime = end - start;
+
+        System.out.println("run2 : " + resultTime * 1e-9 + " sec");
+        return end - start;
+    }
+
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             long test1 = run1();
+            long test2 = run2();
         }
     }
 }
