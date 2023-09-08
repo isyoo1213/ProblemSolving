c171bf526487197b471cbef7ed638dea57c983f2
isyoo1213 <moethns@gmail.com>
2023-09-08 22:47:09 +0900

    *private-BasicPS-ArithmeticSequence-Ps1BR

--- arithmeticSequence/Ps1BR.java
+++ arithmeticSequence/Ps1BR.java
@@ -1,6 +1,8 @@
 package main.sourcecode.basicPS.arithmeticSequence;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 문제
@@ -12,31 +14,22 @@ import java.util.Scanner;
  * 첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
  */
 
-public class Ps1SC {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println(arithmeticSequence(sc.nextInt()));
-        
-        sc.close();
+public class Ps1BR {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        System.out.println(arithmeticSequence(Integer.parseInt(br.readLine())));
     }
 
     private static int arithmeticSequence(int number) {
         int count = 0;
-
-        //100 미만의 수는 모두 등차수열이므로 굳이 반복문을 돌지 않도록 케이스를 나누어 처리리
         if (number < 100) {
-            return number;
+            count = number;
         } else {
-        //100 이상의 수는 최소 한수의 개수가 99개부터 시작
             count = 99;
-
-            // * number의 범위는 100~999 이므로 3자리수로 고정됨
-            // -> 자리수가 고정되지 않은 문제일 경우, while을 통해 접근하는 방법도 생각해봐야함
             for (int i = 100; i <= number; i++) {
                 int hundred = i / 100;
-                int ten = (i / 10) % 10 ;
+                int ten = (i / 10) % 10;
                 int one = i % 10;
 
                 if ((hundred - ten) == (ten - one)) {
